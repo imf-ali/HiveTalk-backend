08c0609c1b2f0a5ff3ef87015d37ed8115647aa4
Fahad Ali <alifahad2210@gmail.com>
2023-08-15 13:28:04 +0530

    user entity added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
+import { UserResolver } from "./resolvers/user";
 import { Context, createContext } from './context'
 
 const main = async () => {
@@ -12,7 +13,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer<Context>({
     schema: await buildSchema({
-      resolvers: [ HelloResolver, PostResolver ],
+      resolvers: [ HelloResolver, PostResolver, UserResolver ],
       validate: false,
     }),
     context: createContext
---
+++ src/resolvers/user.ts
@@ -0,0 +1,96 @@
+import { Context } from 'src/context';
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from 'type-graphql';
+import bcrypt from 'bcryptjs';
+import { User } from '../entities/User';
+
+@InputType()
+class UsernamePasswordInputType {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field?: string;
+
+  @Field()
+  message?: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+
+	@Mutation(() => UserResponse)
+	async register(
+    @Arg('options') options: UsernamePasswordInputType,
+    @Ctx() { prisma } : Context
+  ) : Promise<UserResponse> {
+
+    if(options.username.length <= 3){
+      return {
+        errors: [{
+          field: 'username',
+          message: 'Username length should be greater than 3'
+        }]
+      }
+    }
+
+    if(options.password.length <= 5){
+      return {
+        errors: [{
+          field: 'password',
+          message: 'Password length should be greater than 5'
+        }]
+      }
+    }
+		const user =  await prisma.user.create({
+      data: {
+        username: options.username,
+        password: await bcrypt.hash(options.password, 8)
+      }
+    });
+    return {
+      user
+    }
+	}
+
+  @Mutation(() => UserResponse)
+	async login(
+    @Arg('options') options: UsernamePasswordInputType,
+    @Ctx() { prisma } : Context
+  ) : Promise<UserResponse> {
+		const user = await prisma.user.findFirst({ where: { username: options.username } });
+    if(!user){
+      return {
+        errors: [{
+          field: 'username',
+          message: 'User Not found!'
+        }]
+      }
+    }
+    if(! await bcrypt.compare(options.password, user.password)){
+      return {
+        errors: [{
+          field: 'password',
+          message: 'Password not correct!'
+        }]
+      }
+    }
+    return {
+      user
+    }
+	}
+}
