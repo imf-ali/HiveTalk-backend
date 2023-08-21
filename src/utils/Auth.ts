ed051beeaf34dad6250bcfff67cd879477ccd84c
Fahad Ali <alifahad2210@gmail.com>
2023-08-22 01:00:23 +0530

    env files added

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -1,8 +1,10 @@
 import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+config();
 
 export const createToken = (userId: number) : string => {
 	try {
-		const token = jwt.sign({ _id: userId }, 'graphqlapis');
+		const token = jwt.sign({ _id: userId }, process.env.JWT_SECRET!);
 		return token;
 	} catch (err) {
 		throw err;
@@ -11,7 +13,7 @@ export const createToken = (userId: number) : string => {
 
 export const extractUserId = (token: string): number => {
   try {
-     const decoded = jwt.verify(token, 'graphqlapis');
+     const decoded = jwt.verify(token, process.env.JWT_SECRET!);
      return (<any>decoded)._id;
   } catch (err) {
     throw err;
